ceadcf6832f6bd50ef933f912228122a58b779b1
viktor <vpeche@softserveinc.com>
2020-03-31 18:26:20 +0300

    [facility]: * error handling fixed

--- src/main/java/com/tailoredbrands/pipeline/pattern/jms_to_pub_sub/JmsToPubSubMultiTopicsPipeline.java
+++ src/main/java/com/tailoredbrands/pipeline/pattern/jms_to_pub_sub/JmsToPubSubMultiTopicsPipeline.java
@@ -1,12 +1,14 @@
 package com.tailoredbrands.pipeline.pattern.jms_to_pub_sub;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.tailoredbrands.pipeline.error.ErrorMessage;
 import com.tailoredbrands.pipeline.error.ProcessingException;
 import com.tailoredbrands.pipeline.options.JmsToPubSubOptions;
 import com.tailoredbrands.util.JmsConnectionFactoryBuilder;
 import com.tailoredbrands.util.Peek;
 import com.tailoredbrands.util.json.JsonUtils;
 import com.tailoredbrands.util.validator.PubSubTopicValidator;
+import io.vavr.API;
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Try;
@@ -18,30 +20,21 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessageWithAttributesCoder;
 import org.apache.beam.sdk.io.jms.JmsIO;
 import org.apache.beam.sdk.io.jms.JmsRecord;
+import org.apache.beam.sdk.metrics.Counter;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PCollectionList;
-import org.apache.beam.sdk.values.PCollectionTuple;
-import org.apache.beam.sdk.values.TupleTag;
-import org.apache.beam.sdk.values.TupleTagList;
-import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.JMSException;
 import java.util.HashMap;
 
-import static com.tailoredbrands.pipeline.error.ErrorType.JMS_PAYLOAD_EXTRACTION_ERROR;
-import static com.tailoredbrands.pipeline.error.ErrorType.JSON_TO_PUBSUB_MESSAGE_CONVERSION_ERROR;
-import static com.tailoredbrands.pipeline.error.ErrorType.OBJECT_TO_JSON_CONVERSION_ERROR;
-import static com.tailoredbrands.pipeline.error.ErrorType.XML_TO_OBJECT_CONVERSION_ERROR;
+import static com.tailoredbrands.pipeline.error.ErrorType.*;
 import static com.tailoredbrands.util.Peek.increment;
-import static io.vavr.API.$;
-import static io.vavr.API.Case;
-import static io.vavr.API.Match;
+import static io.vavr.API.*;
 import static java.lang.String.format;
 
 public class JmsToPubSubMultiTopicsPipeline {
@@ -50,9 +43,9 @@ public class JmsToPubSubMultiTopicsPipeline {
 
     public static void main(String[] args) throws JMSException {
         JmsToPubSubOptions options = PipelineOptionsFactory
-                .fromArgs(args)
-                .withValidation()
-                .as(JmsToPubSubOptions.class);
+            .fromArgs(args)
+            .withValidation()
+            .as(JmsToPubSubOptions.class);
         run(options);
     }
 
@@ -61,19 +54,21 @@ public class JmsToPubSubMultiTopicsPipeline {
 
         JmsToPubSubCounter counter = new JmsToPubSubCounter(options.getBusinessInterface());
 
-        TupleTag<PubsubMessage> successTag = new TupleTag<PubsubMessage>() {};
+        TupleTag<PubsubMessage> successTag = new TupleTag<PubsubMessage>() {
+        };
 
-        TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failureTag = new TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>>() {};
+        TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failureTag = new TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>>() {
+        };
 
         PCollectionList pcl = pipeline
-                .apply("Read Messages from JMS",
-                        JmsIO.read()
-                                .withConnectionFactory(
-                                        JmsConnectionFactoryBuilder.build(options, options.getBusinessInterface())
-                                ).withQueue(options.getJmsQueue()))
-                .apply("JMS records counter", increment(counter.jmsRecordsRead))
-                .apply("Extract JMS message payload", extractJmsPayload())
-                .apply("Process Message", JmsToPubSubMultiTopicsProcessorFactory.forType(options.getBusinessInterface()));
+            .apply("Read Messages from JMS",
+                JmsIO.read()
+                    .withConnectionFactory(
+                        JmsConnectionFactoryBuilder.build(options, options.getBusinessInterface())
+                    ).withQueue(options.getJmsQueue()))
+            .apply("JMS records counter", increment(counter.jmsRecordsRead))
+            .apply("Extract JMS message payload", extractJmsPayload())
+            .apply("Process Message", JmsToPubSubMultiTopicsProcessorFactory.forType(options.getBusinessInterface()));
 
         // TODO refactored later and needed proper testing
         List<PCollection> pCollections = List.ofAll(pcl.getAll());
@@ -82,11 +77,11 @@ public class JmsToPubSubMultiTopicsPipeline {
         PubSubTopicValidator topicValidator = new PubSubTopicValidator();
 
         List.of(topics).forEach(topic ->
-                topicValidator.validateTopic(topic).toEither()
-                        .fold(exc -> exc,
-                                validTopic ->
-                                        processAllPC(pCollections, successTag, failureTag, counter, validTopic)
-                        )
+            topicValidator.validateTopic(topic).toEither()
+                .fold(exc -> exc,
+                    validTopic ->
+                        processAllPC(pCollections, successTag, failureTag, counter, options, validTopic)
+                )
         );
 
         return pipeline.run();
@@ -96,15 +91,13 @@ public class JmsToPubSubMultiTopicsPipeline {
                                        TupleTag<PubsubMessage> successTag,
                                        TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failureTag,
                                        JmsToPubSubCounter counter,
+                                       JmsToPubSubOptions options,
                                        String topicName) {
         pcl.filter(pc ->
-                // match by OrgId
-                // TODO replace to OrgID
-                topicName.endsWith(pc.getName())
-        )
-                .forEach(matchedPC ->
-                        processPCollection(matchedPC, successTag, failureTag, counter, topicName)
-                );
+            topicName.endsWith(pc.getName()))
+            .forEach(matchedPC ->
+                processPCollection(matchedPC, successTag, failureTag, counter, options, topicName)
+            );
         return topicName;
     }
 
@@ -112,57 +105,60 @@ public class JmsToPubSubMultiTopicsPipeline {
                                            TupleTag<PubsubMessage> successTag,
                                            TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failureTag,
                                            JmsToPubSubCounter counter,
+                                           JmsToPubSubOptions options,
                                            String outputTopic) {
 
         PCollectionTuple processed = pc
-                .apply("To PubSub Message for " + pc.getName(), toPubSubMessage())
-                .apply("Success | Failure", split(successTag, failureTag));
-
-        // TODO store failures somewhere to replay later?
-        processed
-                .get(failureTag)
-                .apply("Failures counter for " + pc.getName(), increment(counter.totalErrors))
-                .apply("Log failures", logAndCountFailures(counter));
+            .apply("To PubSub Message for " + pc.getName(), toPubSubMessage())
+            .apply("Success | Failure", split(successTag, failureTag));
 
         processed
-                .get(successTag)
-                .setCoder(PubsubMessageWithAttributesCoder.of())
-                .apply("PubSub messages counter for " + pc.getName(), increment(counter.pubsubMessagesWritten))
-                .apply("Write to PubSub for " + pc.getName(), PubsubIO.writeMessages().to(outputTopic));
+            .get(failureTag)
+            .apply("Log failures", logFailures(counter))
+            .apply("Wrap failures", wrapFailures(options.getPatternFullName(), options.getJobName(), options.getBusinessInterface()))
+            .apply("Write failures to dead letter Pubsub", PubsubIO.writeMessages().to(options.getDeadletterPubsubTopic()));
+
+        processed
+            .get(successTag)
+            .setCoder(PubsubMessageWithAttributesCoder.of())
+            .apply("Count and log outbound messages", countAndLogOutbound(counter.pubsubMessagesWritten))
+            .apply("Write to PubSub for " + pc.getName(), PubsubIO.writeMessages().to(outputTopic));
 
     }
 
     static MapElements<JmsRecord, Tuple2<JmsRecord, Try<String>>> extractJmsPayload() {
         return MapElements
-                .into(new TypeDescriptor<Tuple2<JmsRecord, Try<String>>>() {})
-                .via(jmsRecord -> new Tuple2<>(
-                                jmsRecord,
-                                Try
-                                        .of(jmsRecord::getPayload)
-                                        .mapFailure(
-                                                Case($(e -> !(e instanceof ProcessingException)), exc -> new ProcessingException(JMS_PAYLOAD_EXTRACTION_ERROR, exc))
-                                        )
+            .into(new TypeDescriptor<Tuple2<JmsRecord, Try<String>>>() {
+            })
+            .via(jmsRecord -> new Tuple2<>(
+                    jmsRecord,
+                    Try
+                        .of(jmsRecord::getPayload)
+                        .mapFailure(
+                            Case($(e -> !(e instanceof ProcessingException)), exc -> new ProcessingException(JMS_PAYLOAD_EXTRACTION_ERROR, exc))
                         )
-                );
+                )
+            );
     }
 
     static MapElements<Tuple2<JmsRecord, Try<JsonNode>>, Tuple2<JmsRecord, Try<PubsubMessage>>> toPubSubMessage() {
         return MapElements
-                .into(new TypeDescriptor<Tuple2<JmsRecord, Try<PubsubMessage>>>() {})
-                .via(tuple2 ->
-                        tuple2.map2(tryJsonNode ->
-                                tryJsonNode
-                                        .map(jsonNode -> new PubsubMessage(JsonUtils.serializeToBytes(jsonNode), new HashMap<>()))
-                                        .mapFailure(
-                                                Case($(e -> !(e instanceof ProcessingException)), exc -> new ProcessingException(JSON_TO_PUBSUB_MESSAGE_CONVERSION_ERROR, exc))
-                                        )
+            .into(new TypeDescriptor<Tuple2<JmsRecord, Try<PubsubMessage>>>() {
+            })
+            .via(tuple2 ->
+                tuple2.map2(tryJsonNode ->
+                    tryJsonNode
+                        .map(jsonNode -> new PubsubMessage(JsonUtils.serializeToBytes(jsonNode), new HashMap<>()))
+                        .mapFailure(
+                            Case($(e -> !(e instanceof ProcessingException)), exc -> new ProcessingException(JSON_TO_PUBSUB_MESSAGE_CONVERSION_ERROR, exc))
                         )
-                );
+                )
+            );
     }
 
     static ParDo.MultiOutput<Tuple2<JmsRecord, Try<PubsubMessage>>, PubsubMessage> split(
-            TupleTag<PubsubMessage> success,
-            TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failure) {
+        TupleTag<PubsubMessage> success,
+        TupleTag<Tuple2<JmsRecord, Try<PubsubMessage>>> failure) {
         return ParDo.of(new DoFn<Tuple2<JmsRecord, Try<PubsubMessage>>, PubsubMessage>() {
             @ProcessElement
             public void processElement(ProcessContext context) {
@@ -176,17 +172,44 @@ public class JmsToPubSubMultiTopicsPipeline {
         }).withOutputTags(success, TupleTagList.of(failure));
     }
 
-    private static Peek<Tuple2<JmsRecord, Try<PubsubMessage>>> logAndCountFailures(JmsToPubSubCounter counter) {
+    private static Peek<Tuple2<JmsRecord, Try<PubsubMessage>>> logFailures(JmsToPubSubCounter counter) {
         return Peek.each(failure -> {
-            ProcessingException err = (ProcessingException) failure._2.failed().get();
+            val err = (ProcessingException) failure._2.failed().get();
             val detailedCounter = Match(err.getType()).of(
-                    Case($(XML_TO_OBJECT_CONVERSION_ERROR), counter.jmsRecordToObjectErrors),
-                    Case($(OBJECT_TO_JSON_CONVERSION_ERROR), counter.objectToJsonErrors),
-                    Case($(JSON_TO_PUBSUB_MESSAGE_CONVERSION_ERROR), counter.jsonToPubSubErrors),
-                    Case($(), counter.untypedErrors));
+                Case($(JSON_TO_OBJECT_CONVERSION_ERROR), counter.jmsRecordToObjectErrors),
+                Case($(XML_TO_OBJECT_CONVERSION_ERROR), counter.jmsRecordToObjectErrors),
+                Case($(OBJECT_TO_JSON_CONVERSION_ERROR), counter.objectToJsonErrors),
+                Case($(JSON_TO_PUBSUB_MESSAGE_CONVERSION_ERROR), counter.jsonToPubSubErrors),
+                Case($(), counter.untypedErrors));
             detailedCounter.inc();
             counter.totalErrors.inc();
             LOG.error(format("Failed to process Message: %s", failure._1.getPayload()), err);
+        });
+    }
+
+    static MapElements<Tuple2<JmsRecord, Try<PubsubMessage>>, PubsubMessage> wrapFailures(String patternFullName, String jobName, String businessInterface) {
+        return MapElements
+            .into(new TypeDescriptor<PubsubMessage>() {
+            })
+            .via(tuple2 -> {
+                    val err = (ProcessingException) tuple2._2.failed().get();
+                    val errorMessage = new ErrorMessage();
+                    errorMessage.setRawMessage(tuple2._1.getPayload());
+                    errorMessage.setErrMsg(err.getCause().getMessage());
+                    errorMessage.setErrorCode(err.getType().toString());
+                    errorMessage.setFullPatternId(patternFullName);
+                    errorMessage.setJobId(jobName);
+                    errorMessage.setShortPatternId(businessInterface);
+                    errorMessage.setStacktrace(API.List(err.getCause().getStackTrace()).take(10).map(StackTraceElement::toString).mkString("\n"));
+                    return new PubsubMessage(JsonUtils.serializeToBytes(JsonUtils.toJsonNode(errorMessage)), new HashMap<>());
+                }
+            );
+    }
+
+    private static Peek<PubsubMessage> countAndLogOutbound(Counter counter) {
+        return Peek.each(pubsubMessage -> {
+            counter.inc();
+            LOG.info(new String(pubsubMessage.getPayload()));
         });
     }
 }
