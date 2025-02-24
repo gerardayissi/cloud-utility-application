cbd5db214069662ee411d31c90e3da8d9b04ce88
Okele Ayissi <okeleayissih2@nih.gov>
2025-02-23 19:51:20 -0500

    github action deploy cloud function one

--- terragrunt/code/pubsub.tf
+++ terragrunt/code/pubsub.tf
@@ -14,10 +14,10 @@ resource "google_pubsub_subscription" "demo-subscription" {
   depends_on = [google_pubsub_topic.demo-topic]
 }
 
-#resource "google_pubsub_topic" "demo-topic-1" {
-  #name = "demo-topic-1-${var.env}"
-  #project = var.project_id
-#}
+resource "google_pubsub_topic" "demo-topic-1" {
+  name = "demo-topic-1-${var.env}"
+  project = var.project_id
+}
 
 resource "google_pubsub_topic" "demo-topic-2" {
   name = "demo-topic-2-${var.env}"
